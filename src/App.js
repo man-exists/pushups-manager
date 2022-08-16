51c3778cbbbb2da17575353bb4fe3f3b508179f6
man-exists <amityaron100@gmail.com>
2022-08-16 15:54:23 +0300

    Remove Button

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import './app.css'
 
 
 const getDate = () => {
-  var date = new Date()
+    var date = new Date()
 
-  let year = date.getFullYear()
-  let month = (1 + date.getMonth()).toString().padStart(2, '0')
-  let day = date.getDate().toString().padStart(2, '0')
+    let year = date.getFullYear()
+    let month = (1 + date.getMonth()).toString().padStart(2, '0')
+    let day = date.getDate().toString().padStart(2, '0')
 
-  return month + day + year
+    return month + day + year
 }
 
 const pushupsDay = 'pushups' + getDate()
@@ -19,29 +19,38 @@ const cookies = new Cookies()
 
 class App extends React.Component {
 
-  state = {
-    pushups: parseInt(cookies.get(pushupsDay)) || 0
-  }
+    state = {
+        pushups: parseInt(cookies.get(pushupsDay)) || 0
+    }
 
-  addPushup = () => {
-    var newPushups = this.state.pushups + 1
-    this.setState({ pushups: newPushups })
-    cookies.set(pushupsDay, newPushups)
-  }
+    addPushup = () => {
+        var newPushups = this.state.pushups + 1
+        this.setState({ pushups: newPushups })
+        cookies.set(pushupsDay, newPushups)
+    }
 
-  render() {
-    return (
-      <div class="container" onClick={this.addPushup}>
-        <h1>Pushups Manager</h1>
-        <p>a very simple website to track pushups</p>
-        <p>the count restarts everyday at 00:00</p>
-        <p>press the screen with your nose</p>
-        <div class="counterWrapper">
-          <div class="counter" >{this.state.pushups}</div>
-        </div>
-      </div >
-    )
-  }
+    removePushup = () => {
+        var newPushups = this.state.pushups - 1
+        this.setState({ pushups: newPushups })
+        cookies.set(pushupsDay, newPushups)
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.removePushup}>Remove Pushup</button>
+                <div class="container" onClick={this.addPushup}>
+                    <h1>Pushups Manager</h1>
+                    <p>a very simple website to track pushups</p>
+                    <p>the count restarts everyday at 00:00</p>
+                    <p>press the screen with your nose</p>
+                    <div class="counterWrapper">
+                        <div class="counter" >{this.state.pushups}</div>
+                    </div>
+                </div >
+            </div >
+        )
+    }
 }
 
 export default App
