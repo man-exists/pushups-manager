0af9d6fc838603c22b5d6686040f010576a7fb8e
man-exists <amityaron100@gmail.com>
2022-08-16 15:46:16 +0300

    Updates

---
+++ precache-manifest.42470c101fb0e208614ace069c9f3a7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a330362f1abf57df465",
+    "url": "/pushups-manager/static/css/main.b2229e90.chunk.css"
+  },
+  {
+    "revision": "8a330362f1abf57df465",
+    "url": "/pushups-manager/static/js/main.8a330362.chunk.js"
+  },
+  {
+    "revision": "13dd2a8d4a1c849314b9",
+    "url": "/pushups-manager/static/js/1.13dd2a8d.chunk.js"
+  },
+  {
+    "revision": "48ed9757441f3009e0eb",
+    "url": "/pushups-manager/static/js/runtime~main.48ed9757.js"
+  },
+  {
+    "revision": "df5db827ae5ae0408d034949fc46668e",
+    "url": "/pushups-manager/index.html"
+  }
+];
